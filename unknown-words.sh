059ae783137a5621701b06bba633c5026c4b16a6
Josh Soref <jsoref@users.noreply.github.com>
2021-12-16 19:16:51 -0500

    Use to_boolean for boolean inputs

--- unknown-words.sh
+++ unknown-words.sh
@@ -37,7 +37,7 @@ dispatcher() {
       exit 1
       ;;
     push)
-      if [ -n "$INPUT_SUPPRESS_PUSH_FOR_OPEN_PULL_REQUEST" ]; then
+      if to_boolean "$INPUT_SUPPRESS_PUSH_FOR_OPEN_PULL_REQUEST"; then
         pull_request_json=$(mktemp_json)
         pull_request_headers=$(mktemp)
         pull_heads_query="$GITHUB_API_URL/repos/$GITHUB_REPOSITORY/pulls?head=${GITHUB_REPOSITORY%/*}:$GITHUB_REF"
@@ -81,7 +81,7 @@ dispatcher() {
       exec "$spellchecker/check-pull-requests.sh"
       ;;
     issue_comment)
-      if [ -n "$DEBUG" ]; then
+      if to_boolean "$DEBUG"; then
         set -x
       fi
       handle_comment
@@ -166,17 +166,6 @@ comment_task() {
   more_misspellings
 }
 
-to_boolean() {
-  case "$1" in
-    1|true|TRUE)
-      true
-    ;;
-    *)
-      false
-    ;;
-  esac
-}
-
 should_patch_head() {
   if [ ! -d "$bucket/$project" ]; then
     # if there is no directory in the merged state, then adding files into it
@@ -241,7 +230,7 @@ react_comment_and_die() {
     res=0
     comment "$COMMENTS_URL" "$PAYLOAD" > /dev/null || res=$?
     if [ $res -gt 0 ]; then
-      if [ -z "$DEBUG" ]; then
+      if ! to_boolean "$DEBUG"; then
         echo "failed posting to $COMMENTS_URL"
         cat "$PAYLOAD"
       fi
@@ -414,7 +403,7 @@ handle_comment() {
   res=0
   comment "$comment_url" > $comment || res=$?
   if [ $res -gt 0 ]; then
-    if [ -z "$DEBUG" ]; then
+    if ! to_boolean "$DEBUG"; then
       echo "failed to retrieve $comment_url"
     fi
     return $res
@@ -502,7 +491,7 @@ define_variables() {
   fi
   bucket=${INPUT_BUCKET:-$bucket}
   project=${INPUT_PROJECT:-$project}
-  if [ -n "$junit" ]; then
+  if to_boolean "$junit"; then
     quit_without_error=1
   fi
   if [ -z "$bucket" ] && [ -z "$project" ] && [ -n "$INPUT_CONFIG" ]; then
@@ -823,14 +812,14 @@ set_up_reporter() {
   mkdir -p .git
   cp $spellchecker/reporter.json .git/
   echo "::add-matcher::.git/reporter.json"
-  if [ -n "$DEBUG" ]; then
+  if to_boolean "$DEBUG"; then
     echo 'env:'
     env|sort
   fi
   if [ -z "$GITHUB_EVENT_PATH" ] || [ ! -s "$GITHUB_EVENT_PATH" ]; then
     GITHUB_EVENT_PATH=/dev/null
   fi
-  if [ -n "$DEBUG" ]; then
+  if to_boolean "$DEBUG"; then
     echo 'GITHUB_EVENT_PATH:'
     cat $GITHUB_EVENT_PATH
   fi
@@ -952,7 +941,7 @@ set_up_files() {
 
 welcome() {
   echo "Checking spelling..."
-  if [ -n "$DEBUG" ]; then
+  if to_boolean "$DEBUG"; then
     begin_group 'Excluded paths'
     if [ -e "$excludes" ]; then
       echo 'Excluded paths:'
@@ -981,7 +970,7 @@ run_spell_check() {
   begin_group 'Spell check files'
   file_list=$(mktemp)
   (
-    if [ -n "$INPUT_ONLY_CHECK_CHANGED_FILES" ]; then
+    if to_boolean "$INPUT_ONLY_CHECK_CHANGED_FILES"; then
       COMPARE=$(cat "$GITHUB_EVENT_PATH" | jq -r '.compare // empty' 2>/dev/null)
       if [ -n "$COMPARE" ]; then
         BEFORE=$(echo "$COMPARE" | perl -ne 'if (m{/compare/(.*)\.\.\.}) { print $1; } elsif (m{/commit/([0-9a-f]+)$}) { print "$1^"; };')
@@ -1002,7 +991,7 @@ run_spell_check() {
     fi
   ) |\
     "$spellchecker/exclude.pl" > "$file_list"
-  if [ -n "$INPUT_CHECK_FILE_NAMES" ]; then
+  if to_boolean "$INPUT_CHECK_FILE_NAMES"; then
     check_file_names="$spellchecker/paths-of-checked-files.txt"
     cat "$file_list" | tr "\0" "\n" > "$check_file_names"
     echo "$check_file_names" | tr "\n" "\0" >> "$file_list"
@@ -1091,7 +1080,7 @@ to_publish_expect() {
 }
 
 remove_items() {
-  if [ -n "$INPUT_ONLY_CHECK_CHANGED_FILES" ]; then
+  if to_boolean "$INPUT_ONLY_CHECK_CHANGED_FILES"; then
     echo "<!-- Because only_check_changed_files is active, checking for obsolete items cannot be performed-->"
   else
     if [ -z "$patch_remove" ]; then
@@ -1305,7 +1294,7 @@ spelling_body() {
 quit() {
   echo "::remove-matcher owner=check-spelling::"
   cat $output_variables
-  if [ -n "$quit_without_error" ]; then
+  if to_boolean "$quit_without_error"; then
     exit
   fi
   exit $1
@@ -1315,7 +1304,7 @@ body_to_payload() {
   BODY="$1"
   PAYLOAD=$(mktemp)
   echo '{}' | jq --rawfile body "$BODY" '.body = $body' > $PAYLOAD
-  if [ -n "$DEBUG" ]; then
+  if to_boolean "$DEBUG"; then
     cat $PAYLOAD >&2
   fi
 }
@@ -1451,14 +1440,14 @@ post_commit_comment() {
         res=0
         comment "$COMMENTS_URL" "$PAYLOAD" > $response || res=$?
         if [ $res -gt 0 ]; then
-          if [ -z "$DEBUG" ]; then
+          if ! to_boolean "$DEBUG"; then
             echo "failed posting to $COMMENTS_URL"
             cat "$PAYLOAD"
           fi
           no_patch=1
         fi
 
-        if [ -n "$DEBUG" ]; then
+        if to_boolean "$DEBUG"; then
           cat $response
         fi
         COMMENT_URL=$(jq -r '.url // empty' $response)
@@ -1489,11 +1478,11 @@ post_commit_comment() {
             body_to_payload $BODY
             comment "$COMMENT_URL" "$PAYLOAD" "PATCH" > $response || res=$?
             if [ $res -gt 0 ]; then
-              if [ -z "$DEBUG" ]; then
+              if ! to_boolean "$DEBUG"; then
                 echo "Failed to patch $COMMENT_URL"
               fi
             fi
-            if [ -n "$DEBUG" ]; then
+            if to_boolean "$DEBUG"; then
               cat $response
             fi
           fi
